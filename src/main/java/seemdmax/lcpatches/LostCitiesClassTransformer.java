0c901a287997a502b1c03dbddb10d4a5c41f7878
Nick1st <54022594+Nick1st@users.noreply.github.com>
2021-05-04 07:42:18 +0200

    Add files via upload

--- src/main/java/seemdmax/lcpatches/LostCitiesClassTransformer.java
+++ src/main/java/seemdmax/lcpatches/LostCitiesClassTransformer.java
@@ -2,10 +2,13 @@ package seemdmax.lcpatches;
 
 import static org.objectweb.asm.Opcodes.ALOAD;
 import static org.objectweb.asm.Opcodes.IFGT;
+import static org.objectweb.asm.Opcodes.IFLE;
+import static org.objectweb.asm.Opcodes.IFNE;
 import static org.objectweb.asm.Opcodes.ILOAD;
 import static org.objectweb.asm.Opcodes.INVOKEINTERFACE;
 import static org.objectweb.asm.Opcodes.INVOKESTATIC;
 import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.objectweb.asm.Opcodes.GETFIELD;
 
 import java.util.Arrays;
 
@@ -15,6 +18,9 @@ import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.VarInsnNode;
@@ -23,7 +29,7 @@ import mcjty.lostcities.api.ILostCityBuilding;
 import net.minecraft.launchwrapper.IClassTransformer;
 
 public class LostCitiesClassTransformer implements IClassTransformer {
-	private static final String[] classesBeingTransformed = { "mcjty.lostcities.dimensions.world.lost.BuildingInfo" };
+	private static final String[] classesBeingTransformed = { "mcjty.lostcities.dimensions.world.lost.BuildingInfo", "mcjty.lostcities.dimensions.world.terraingen.LostCitiesTerrainGenerator"};
 
 	@Override
 	public byte[] transform(String name, String transformedName, byte[] classBeingTransformed) {
@@ -37,22 +43,67 @@ public class LostCitiesClassTransformer implements IClassTransformer {
 		try {
 			ClassNode classNode = new ClassNode();
 			ClassReader classReader = new ClassReader(classBeingTransformed);
-			classReader.accept(classNode, 0);
+			classReader.accept(classNode, 2);
 
 			System.out.println("Transforming " + classesBeingTransformed[index] + " Is Obf: " + isObfuscated);
 			switch (index) {
 			case 0:
 				transformLCCellars(classNode, isObfuscated);
 				break;
+			case 1:
+				transformBuildingBorders(classNode, isObfuscated);
+				break;
 			}
 
-			ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS /* | ClassWriter.COMPUTE_FRAMES */);
+			ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS /*| ClassWriter.COMPUTE_FRAMES*/);
 			classNode.accept(classWriter);
 			return classWriter.toByteArray();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return classBeingTransformed;
+	}
+
+	private static void transformBuildingBorders(ClassNode terrainGenClass, boolean isObfuscated) {
+		final String METHOD = isObfuscated ? "generateBuilding" : "generateBuilding";
+		final String METHOD_DESC = isObfuscated
+				? "(Lmcjty/lostcities/dimensions/world/lost/BuildingInfo;Lmcjty/lostcities/dimensions/world/ChunkHeightmap;)V"
+				: "(Lmcjty/lostcities/dimensions/world/lost/BuildingInfo;Lmcjty/lostcities/dimensions/world/ChunkHeightmap;)V";
+		
+		for (MethodNode method : terrainGenClass.methods) {
+			if (method.name.equals(METHOD) && method.desc.equals(METHOD_DESC)) {
+				System.out.println("Found method in BuildingInfo to transform");
+				AbstractInsnNode targetNode = null;
+				for (AbstractInsnNode instruction : method.instructions.toArray()) {
+					if (instruction.getOpcode() == ALOAD) {
+						if (((VarInsnNode) instruction).var == 1 & instruction.getNext().getOpcode() == GETFIELD & instruction.getNext().getNext().getOpcode() == IFLE) {
+							System.out.println("Matched");
+							targetNode = instruction.getNext().getNext();
+							break;
+						}
+					}
+				}
+				if (targetNode != null) {
+					System.out.println("Target Node valid");
+
+					InsnList toInsert = new InsnList();
+					toInsert.add(new VarInsnNode(ALOAD, 1));
+					toInsert.add(
+							new MethodInsnNode(INVOKEVIRTUAL, "mcjty/lostcities/dimensions/world/lost/BuildingInfo",
+									"getBuildingType", "()Ljava/lang/String;", false));
+					toInsert.add(new LdcInsnNode("#NOBORDER"));
+					toInsert.add(
+							new MethodInsnNode(INVOKEVIRTUAL, "java/lang/String", "endsWith", "(Ljava/lang/String;)Z", false));
+					toInsert.add(new JumpInsnNode(IFNE, ((JumpInsnNode) targetNode).label));
+
+					method.instructions.insert(targetNode, toInsert);
+					System.out.println("Transform done!");
+				} else {
+					System.out.println("Something went wrong transforming LostCitiesTerrainGenerator!");
+				}
+			}
+		}
+		
 	}
 
 	private static void transformLCCellars(ClassNode buildingInfoClass, boolean isObfuscated) {
